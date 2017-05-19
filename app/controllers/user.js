91d67a6ac80b548214e6029daaf1149d9ca6f2c8
andresd24 <andresdev@gmail.com>
2017-05-19 12:32:13 -0500

    minor change to UX

--- app/controllers/user.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Controller.extend({
-  init () {
-    let users = this.get("users");
-// results is returning undefined
-    // users is also undefined
-  }
-});
